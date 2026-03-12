113b774d4d72e7e99da700bff0e80497f1714963
Mohammed Ashraf <mohamedasharf55@gmail.com>
2026-03-13 00:49:08 +0200

    add file web experience

---
+++ lib/features/experience/presentation/screens/web_experience_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class WebExperienceScreen extends StatelessWidget {
+  const WebExperienceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Text('Experience Screen for Web'),
+        ),
+      ),
+    );
+  }
+}
