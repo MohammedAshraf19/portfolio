4ca37958bf7101c304536ae96ed60dc55e13acfc
Mohammed Ashraf <mohamedasharf55@gmail.com>
2026-03-26 18:22:08 +0200

    Create Experience Item Model

---
+++ lib/features/experience/models/experience_item_model.dart
@@ -0,0 +1,19 @@
+class ExperienceItemModel {
+  String? description;
+  String? jobTitle;
+  String? companyName;
+  String? duration;
+  String? location;
+  String? companyLogo;
+  List<String>? photo;
+
+  ExperienceItemModel({
+    required this.description,
+    required this.jobTitle,
+    required this.companyName,
+    required this.duration,
+    required this.location,
+    required this.companyLogo,
+    required this.photo,
+  });
+}
